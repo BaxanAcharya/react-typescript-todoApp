1b6824a57e310bcde469670acdff452e66942d31
BaxanAcharya <baxanacharya@gmail.com>
2020-12-16 11:23:09 +0545

    added toggle functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoListItem from './component/TodoListItem';
 
 
 
-const todos: Array<Todo>=[
+const initialTodos: Array<Todo> = [
   {
-  text:'Walk the dog',
-  complete:true
-},
-{
-  text:'Brush the teeth',
-  complete:false
-},
-{
-  
-  text:'Wash your face',
-  complete:true
-},
-{
-  text:'Do light excercise',
-  complete:true
-},
+    text: 'Walk the dog',
+    complete: true
+  },
+  {
+    text: 'Brush the teeth',
+    complete: false
+  },
+  {
+
+    text: 'Wash your face',
+    complete: true
+  },
+  {
+    text: 'Do light excercise',
+    complete: true
+  },
 ]
-const App: React.FC=()=>{
-return <div>
-  
-</div>
+const App: React.FC = () => {
+
+  const [todos, setTodos] = useState(initialTodos)
+
+  const toggleTodo: ToggleTodo = selectedTodo => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete
+        }
+      }
+      return todo
+    })
+
+    setTodos(newTodos)
+  }
+  return <div>
+    {
+      todos.map((todo, index) => {
+        return <TodoListItem todo={todo} key={index} toggleTodo={toggleTodo} />
+      })
+    }
+  </div>
 }
 export default App
--- src/component/TodoListItem.tsx
+++ src/component/TodoListItem.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import './TodoListitem.css'
 
-
-interface TodoListItemProps{
-    todo: Todo
+interface TodoListItemProps {
+    todo: Todo;
+    toggleTodo: ToggleTodo
 }
 
-const TodoListItem: React.FC<TodoListItemProps> =({todo})=>{
-return <li><label><input type="checkbox" checked={todo.complete} /></label>{todo.text}</li>
+const TodoListItem: React.FC<TodoListItemProps> = ({ todo, toggleTodo }) => {
+    console.log(todo)
+    return <> <li>
+        <label className={todo.complete ? 'complete' : undefined}>
+            <input type="checkbox" checked={todo.complete} onChange={() => toggleTodo(todo)} className="point" />
+        </label>{todo.text}  <strong>{todo.complete ? "done" : 'do it now'}</strong>
+    </li>
+        <br /> <hr /></>
 }
 
 export default TodoListItem;
